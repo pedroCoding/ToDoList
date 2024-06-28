79fdf77b43f71b47e49c644b1ef71b4e937df9f7
pedroCoding <falecompedrodev@gmail.com>
2024-06-28 13:48:38 -0300

    first commit

---
+++ src/main/java/br/com/pedroCoding/todolist/todolistapplication.java
@@ -0,0 +1,13 @@
+package br.com.pedroCoding.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class todolistapplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(todolistapplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/pedroCoding/todolist/user/UserModel.java
@@ -0,0 +1,17 @@
+package br.com.pedroCoding.todolist.user;
+
+public class UserModel {
+    
+    public String name;
+    public String username;
+    public String password;
+
+    /*
+     *
+     * public void setUsername(String username){
+        this.username = username;
+    }
+     */
+    
+
+}
