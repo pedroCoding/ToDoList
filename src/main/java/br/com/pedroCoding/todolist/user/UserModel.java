81ddf2d0c077bd8d3f7d5be3bdff2da5c56b073b
pedroCoding <falecompedrodev@gmail.com>
2024-06-28 19:36:59 -0300

    Adicionando campos a tabela + importação de bibliotecas

--- src/main/java/br/com/pedroCoding/todolist/user/UserModel.java
+++ src/main/java/br/com/pedroCoding/todolist/user/UserModel.java
@@ -1,16 +1,29 @@
 package br.com.pedroCoding.todolist.user;
 
+import java.util.UUID;
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Data
+@Entity(name = "tb_users") //Nome da Tabela - Tem que ser diferente de users
 public class UserModel {
+
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
     
     private String name;
     private String username;
     private String password;
 
-    
-    
-    
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
 
 }
