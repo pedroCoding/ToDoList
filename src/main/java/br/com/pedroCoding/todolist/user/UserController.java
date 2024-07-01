afeced02cd71abf0257597241f7fce58cc912501
pedroCoding <falecompedrodev@gmail.com>
2024-07-01 13:56:14 -0300

    Adicionando função para verificação de ID do usuário. Validação!

--- src/main/java/br/com/pedroCoding/todolist/user/UserController.java
+++ src/main/java/br/com/pedroCoding/todolist/user/UserController.java
@@ -1,17 +1,23 @@
 package br.com.pedroCoding.todolist.user;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/users")
-
 public class UserController {
+
+    @Autowired
+    private IUserRepository userRepository;
+
     
+
     @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.getName());
+    public UserModel create(@RequestBody UserModel userModel){
+        var userCreated = this.userRepository.save(userModel);
+        return userCreated;
     }
 }
